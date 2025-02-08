32f5ef6913ef5b79c1430618a203381b6fd8ac1b
Griplan <2424015851@qq.com>
2025-02-09 04:01:50 +0800

    container

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
-    <h1>我是Main</h1>
-    <router-view></router-view>
-  </div>
+    <el-container>
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>
+         <div>
+            <h1>我是Main</h1>
+            <router-view></router-view>
+        </div>
+    </el-main>
+  </el-container>
+</el-container>
+
 </template>
 
 <script>
