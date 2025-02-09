7fbc5d55d80b3af92c1f4194bd8b91956be18300
Griplan <2424015851@qq.com>
2025-02-09 15:48:09 +0800

    菜单样式与less引入

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -2,10 +2,12 @@
   <el-menu
     default-active="1-4-1"
     class="el-menu-vertical-demo"
-    @open="handleOpen"
-    @close="handleClose"
     :collapse="isCollapse"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
   >
+    <h3>通用后台管理系统</h3>
     <el-menu-item
       v-for="item in noChildren"
       :key="item.name"
@@ -30,10 +32,20 @@
   </el-menu>
 </template>
 
-<style>
+<style lang="less" scoped>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   min-height: 400px;
+}
+.el-menu {
+  height: 100vh;
+  h3 {
+    color: #fff;
+    text-align: center;
+    line-height: 48px;
+    font-size: 16px;
+    font-weight: 400;
+  }
 }
 </style>
 
