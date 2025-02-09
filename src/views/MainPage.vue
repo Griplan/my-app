4a9a3fecfc266aaf89b086294c9198057ed9f04e
Griplan <2424015851@qq.com>
2025-02-09 14:16:35 +0800

    aside init

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,6 +1,8 @@
 <template>
     <el-container>
-  <el-aside width="200px">Aside</el-aside>
+  <el-aside width="200px">
+    <CommonAside/>
+  </el-aside>
   <el-container>
     <el-header>Header</el-header>
     <el-main>
@@ -15,9 +17,13 @@
 </template>
 
 <script>
+import CommonAside from '../components/CommonAside.vue'
 export default {
   data(){
     return {}
+  },
+  components:{
+    CommonAside
   }
 }
 </script>
