9ed4f5a45361143bdfbdc5ca5480ec3f8c0dc645
Griplan <2424015851@qq.com>
2025-02-11 02:07:00 +0800

    Home组件布局

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,63 @@
 <template>
-  <div>
-    <h1>我是Home</h1>
-  </div>
+  <el-row>
+    <el-col :span="8" style="padding-right: 10px">
+      <el-card class="box-card">
+        <div class="user">
+          <img src="../assets/头像.jpg" alt="" />
+          <div class="userinfo">
+            <p class="name">Admin</p>
+            <p class="access">超级管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登录时间：<span>2021-7-19</span></p>
+          <p>上次登录地点：<span>武汉</span></p>
+        </div>
+      </el-card></el-col
+    >
+    <el-col :span="16"><div class="grid-content bg-purple-light"></div></el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
-  data(){
-    return {}
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="less" scoped>
+.user {
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #ccc;
+  display: flex;
+  align-items: center;
+  img {
+    margin-right: 40px;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+  }
+  .userinfo {
+    .name {
+      font-size: 32px;
+      margin-bottom: 10px;
+    }
+    .access {
+      color: #999999;
+    }
   }
 }
-</script>
-
+.login-info {
+  p {
+    line-height: 28px;
+    font-size: 14px;
+    color: #999999;
+    span {
+      color: #666666;
+      margin-left: 60px;
+    }
+  }
+}
+</style>
